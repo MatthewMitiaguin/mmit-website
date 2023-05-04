76bcf292fde34aa41ee5cae860c20712eed10adc
Matthew Mitiaguin <mmitiaguin@gmail.com>
2023-05-04 22:15:43 +1000

    Moving styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,6 @@
 import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
+import './styles.css'
 
 const links = [
   {
@@ -87,31 +28,26 @@ const links = [
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
+    <main className="pageStyles">
+      <h1 className="headingStyles">
         Matthew Mitiaguin
         <br />
-        <span style={headingAccentStyles}>— my homepage! 🎉😎🎉😎🎉</span>
+        <span className="headingAccentStyles">— my homepage! 🎉😎🎉😎🎉</span>
       </h1>
-      <p style={paragraphStyles}>
+      <p className="paragraphStyles">
         Skiing enthusiast & lifelong software engineer learner
       </p>
-      <ul style={listStyles}>
+      <ul className="listStyles">
         {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+          <li key={link.url} className="listItemStyles">
             <span>
               <a
-                style={linkStyle}
+                className="linkStyle"
                 href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
               >
                 {link.text}
               </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
+              <p className="descriptionStyle">{link.description}</p>
             </span>
           </li>
         ))}
@@ -122,4 +58,4 @@ const IndexPage: React.FC<PageProps> = () => {
 
 export default IndexPage
 
-export const Head: HeadFC = () => <title>Home Page</title>
+export const Head: HeadFC = () => <title>Matt Mitiaguin</title>
